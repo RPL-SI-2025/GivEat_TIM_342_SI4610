5a5ad6c9e964db53da0c3bd48b3836a803ce6d1f
Rilgalinggaw <rilgalingaa@gmail.com>
2025-04-18 21:50:10 +0700

    S3G-10 Membuat fitur login dan regis yang sudah multi-user (user, admin dan mitra)

---
+++ GivEat/app/Http/Controllers/Mitra/MitraController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Mitra;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class MitraController extends Controller
+{
+    public function index()
+    {
+        return view('mitra.dashboard');
+    }
+}
--- GivEat/routes/web.php
+++ GivEat/routes/web.php
@@ -1,13 +1,42 @@
 <?php
 
+Use App\Http\Controllers\User\UserController;
+use App\Http\Controllers\Mitra\mitraController;
+use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Admin\FaqController;
+
+
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::prefix('admin')->name('admin.')->group(function () {
-    Route::resource('faq', FaqController::class);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+require __DIR__.'/auth.php';
+
+// User Routes
+Route::middleware(['auth', 'userMiddleware'])->group(function () {
+    
+    Route::get('dashboard',[UserController::class, 'index'])->name('dashboard');
+
+});
+
+// Mitra Routes
+Route::middleware(['auth', 'mitraMiddleware'])->group(function () {
+    
+    Route::get('/mitra/dashboard',[MitraController::class, 'index'])->name('mitra.dashboard');
+
+});
+
+// Admin Routes
+Route::middleware(['auth', 'adminMiddleware'])->group(function () {
+    
+    Route::get('/admin/dashboard',[AdminController::class, 'index'])->name('admin.dashboard');
+
+});
