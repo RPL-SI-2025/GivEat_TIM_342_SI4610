a1f688738096cc4c6289b5505b656e4ee57ce938
rilga <rilgalingga5@gmail.com>
2025-05-09 14:53:08 +0700

    S3G-13 Membuat Create dan Read data dari CRUD Manajemen Mitra 2

---
+++ GivEat/app/Http/Controllers/DashboardController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Donation;
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    public function index()
+    {
+        $availableDonations = Donation::where('status', 'available')
+            ->with(['partner', 'category'])
+            ->latest()
+            ->take(4)
+            ->get();
+
+        return view('dashboard', [
+            'availableDonations' => $availableDonations,
+            'restaurants' => ['cheffest', 'bistro', 'dapur', 'resto1', 'resto2', 'selera'] // Keep existing restaurant data
+        ]);
+    }
+}
