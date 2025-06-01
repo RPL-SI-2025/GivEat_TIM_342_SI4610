664e1b13b9fbae53b2ab9155867c1882a3801006
bintangpreciosa <bintangpreciosa2@gmail.com>
2025-06-01 20:32:19 +0700

    S3G-17 Update Test Case

--- GivEat/tests/Browser/KlaimMakananTest.php
+++ GivEat/tests/Browser/KlaimMakananTest.php
@@ -14,13 +14,24 @@ class KlaimMakananTest extends DuskTestCase
     public function testExample(): void
     {
         $this->browse(function (Browser $browser) {
-            $browser->visit('/donations/1')
+            $user = \App\Models\User::where('email','bintang@gmail.com')->first();
+            $browser->loginAs($user)
+                    ->visit('/donations/1')
+                    ->pause(2000)
                     ->assertSee('Sop Ayam dan Sayur')
+                    ->pause(2000)
                     ->press('Ambil Makananmu')
+                    ->pause(2000)
                     ->assertPathIs('/claim/success/default-slug')
+                    ->pause(2000)
                     ->press('Kembali ke Beranda')
+                    ->pause(2000)
                     ->press('Batalkan Pemesanan')
-                    ->click('a[href^="https://maps.app.goo.gl"]');
+                    ->pause(2000)
+                    ->acceptDialog()
+                    ->pause(2000)
+                    ->click('a[href^="https://maps.app.goo.gl/iDKgFx7JW1ppqdEB6"]')
+                    ->pause(5000);
                     
         });
     }
